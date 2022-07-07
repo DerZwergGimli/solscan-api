bf55c958063f7a29dec140105a68f377925b6a88
DZWG <paule22@protonmail.com>
2022-07-07 23:00:37 +0200

    clean up

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,4 @@
 mod test_api_base;
-mod test_serde;
-mod test_integration;
+
 mod test_mock_api;
 mod test_endpoints;
--- src/tests/test_mock_api.rs
+++ src/tests/test_mock_api.rs
@@ -1,83 +1,15 @@
 #![allow(unused_variables)]
 
 #[cfg(test)]
-mod test_mock_api {
+mod test_error_codes {
     use httpmock::MockServer;
     use httpmock::prelude::*;
     use serde_json::json;
 
     use crate::solscan::SolscanAPI;
 
-    /*    fn build_mock_server_block() -> MockServer {
-                                            let server = MockServer::start();
-
-                                            let mock_block = server.mock(|when, then| {
-                                                when.method(GET)
-                                                    .path("/block/last");
-                                                then.status(200)
-                                                    .header("content-type", "text/html")
-                                                    .json_body(json!([
-                                                                              {
-                                                                                "currentSlot": 140470240,
-                                                                                "result": {
-                                                                                  "blockHeight": 126891281,
-                                                                                  "blockTime": 1657124585,
-                                                                                  "blockhash": "HijFFgfAfeawaKUZ3UdBSw5Qyi5EfhwszH6S9CkMa6nC",
-                                                                                  "parentSlot": 140470239,
-                                                                                  "previousBlockhash": "FjxsV8xmR5AB4AU8sBFJDvm8Cexgx8BeXaiarNvkKFH",
-                                                                                  "feeRewards": 5883517,
-                                                                                  "validator": "2mMGsb5uy1Q4Dvezr8HK2E8SJoChcb2X7b61tJPaVHHd",
-                                                                                  "transactionCount": 2322
-                                                                                }
-                                                                              }]));
-                                            });
-                                            server
-                                        }*/
-
-    fn build_mock_server_account_tokens() -> MockServer {
-        let server = MockServer::start();
-
-        let mock_block = server.mock(|when, then| {
-            when.method(GET)
-                .path("/account/tokens")
-                .query_param("account", "So11111111111111111111111111111111111111112");
-            then.status(200)
-                .header("content-type", "text/html")
-                .json_body(json!([
-                                      {
-                                        "tokenAddress": "Fh6Vvw44wb6n5honzAfvrae6iq7FSRiKzMB6runL8csb",
-                                        "tokenAmount": {
-                                          "amount": "1",
-                                          "decimals": 0,
-                                          "uiAmount": 1,
-                                          "uiAmountString": "1"
-                                        },
-                                        "tokenAccount": "Cexn2xqP9ruDzp2Pmrg4ahFBMKHyULdjwnCeVBSGQkFh",
-                                        "tokenName": "",
-                                        "tokenIcon": "",
-                                        "rentEpoch": 324,
-                                        "lamports": 2039280
-                                      }]));
-        });
-        server
-    }
-
-    /*    fn build_mock_server_error() -> MockServer {
-            let server = MockServer::start();
-
-            let mock_block = server.mock(|when, then| {
-                when.method(GET)
-                    .path("/block/last");
-                then.status(400)
-                    .header("content-type", "text/html");
-            });
-            server
-        }*/
-
-    //region ErrorCodesTest
-
     #[tokio::test]
-    async fn test_mock_api_200() {
+    async fn test_error_code_200_success() {
         let server = MockServer::start();
 
         let mock_block = server.mock(|when, then| {
@@ -92,7 +24,7 @@ mod test_mock_api {
     }
 
     #[tokio::test]
-    async fn test_mock_api_400() {
+    async fn test_error_code_400_success() {
         let server = MockServer::start();
 
         let mock_block = server.mock(|when, then| {
@@ -107,7 +39,7 @@ mod test_mock_api {
     }
 
     #[tokio::test]
-    async fn test_mock_api_429() {
+    async fn test_error_code_429_success() {
         let server = MockServer::start();
 
         let mock_block = server.mock(|when, then| {
@@ -122,7 +54,7 @@ mod test_mock_api {
     }
 
     #[tokio::test]
-    async fn test_mock_api_500() {
+    async fn test_error_code_500_success() {
         let server = MockServer::start();
 
         let mock_block = server.mock(|when, then| {
@@ -135,42 +67,4 @@ mod test_mock_api {
         let result = solscan_api.ping_status(Some("/test".to_string())).await.unwrap();
         assert_eq!(result, 500)
     }
-
-    //endregion
-
-    /*    #[tokio::test]
-        async fn test_mock_api_fetch_block_last_success() {
-            let server = build_mock_server_block();
-
-            let solscan_api = SolscanAPI::new_with_url(server.url(""));
-            let result = solscan_api.get_block_last().await.unwrap();
-            assert_eq!(result.len(), 1)
-        }*/
-
-    #[tokio::test]
-    async fn test_mock_api_fetch_account_tokens_last_success() {
-        let server = build_mock_server_account_tokens();
-
-        let solscan_api = SolscanAPI::new_with_url(server.url(""));
-        let result = solscan_api.get_account_tokens("So11111111111111111111111111111111111111112").await.unwrap();
-        assert_eq!(result.len(), 1)
-    }
-
-    /*#[tokio::test]
-    async fn test_mock_api_fetch_account_token_parse_error() {
-        let server = build_mock_server_block();
-
-        let solscan_api = SolscanAPI::new_with_url(server.url(""));
-        let result = solscan_api.solscan_fetch::<Vec<Transaction>>(SolscanEndpoints::BlockLast, None, None, None, None).await;
-        assert!(result.is_err())
-    }
-
-    #[tokio::test]
-    async fn test_mock_api_fetch_account_token_parse_error2() {
-        let server = build_mock_server_error();
-
-        let solscan_api = SolscanAPI::new_with_url(server.url(""));
-        let result = solscan_api.solscan_fetch::<Vec<Transaction>>(SolscanEndpoints::BlockLast, None, None, None, None).await;
-        assert!(result.is_err())
-    }*/
 }
