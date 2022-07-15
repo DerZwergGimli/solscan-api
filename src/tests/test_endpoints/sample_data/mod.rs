d63f1f473dc8397a057281d8bdfb5f1dd0501791
DZWG <paule22@protonmail.com>
2022-07-15 13:53:43 +0200

    fix test sample_token_meta

--- src/tests/test_endpoints/sample_data/mod.rs
+++ src/tests/test_endpoints/sample_data/mod.rs
@@ -1,3 +1,2 @@
 pub mod sample_token_holders;
 pub mod sample_token_meta;
-pub mod sample_market_token;
--- src/tests/test_endpoints/test_token_meta.rs
+++ src/tests/test_endpoints/test_token_meta.rs
@@ -2,14 +2,21 @@
 
 #[cfg(test)]
 mod test_token_meta {
+    use std::fs;
+
+    use assert_json_diff::{assert_json_matches, CompareMode, Config, NumericMode};
     use httpmock::MockServer;
     use httpmock::prelude::*;
+    use serde_json::{json, Value};
 
     use crate::solscan::SolscanAPI;
-    use crate::tests::test_endpoints::sample_data::sample_token_meta::SAMPLE_TOKEN_META;
 
     #[tokio::test]
     async fn test_token_meta_success() {
+        let json_data_file = fs::read_to_string("./src/tests/test_endpoints/sample_data/sample_token_meta.json").expect("Unable to read file");
+        let json_data: Value = serde_json::from_str(&json_data_file).expect("JSON does not have correct format.");
+        let config = Config::new(CompareMode::Strict).numeric_mode(NumericMode::AssumeFloat);
+
         let server = MockServer::start();
         let mock_block = server.mock(|when, then| {
             when.method(GET)
@@ -17,11 +24,12 @@ mod test_token_meta {
                 .query_param("tokenAddress", "So11111111111111111111111111111111111111112");
             then.status(200)
                 .header("content-type", "text/html")
-                .body(SAMPLE_TOKEN_META);
+                .json_body(json_data.clone());
         });
 
         let solscan_api = SolscanAPI::new_with_url(server.url(""));
-        let result = solscan_api.get_token_meta("So11111111111111111111111111111111111111112").await;
-        assert_eq!(result.is_ok(), true)
+        let result = solscan_api.get_token_meta("So11111111111111111111111111111111111111112").await.unwrap();
+
+        assert_json_matches!(json!(&result), json_data, config)
     }
 }
