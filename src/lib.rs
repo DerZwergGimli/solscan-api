6a1ece2e7fdd238f13b521795c1d3e4966599f37
DZWG <paule22@protonmail.com>
2022-07-15 15:49:32 +0200

    added general doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
 //! # SOLSCAN-API
 //! This lib represent a wrapper for the SolcanAPI
+//!
+//! # Solscan API
+//!
+//! [Solscan-Public-API](https://public-api.solscan.io/docs/#/)
+//! `Default Limit: 150 requests/ 30 seconds, 100k requests / day`
+//!
+//!
+//! # Example
+//! Fetching last 10 Blocks form Solana-Blockchain via SolscanAPI
+//! ```
+//!async fn main() {
+//!     let solscan_api = solscan_api::solscan::SolscanAPI::new();
+//!     let result = solscan_api.get_block_last(Some(10)).await.unwrap();
+//!
+//!     println("{:?}", result)
+//! }
+//! ```
 
 pub mod solscan;
 mod r#const;
